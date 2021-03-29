2d809f2c4b00b578c3d8933f2e0ec5d63e86b92d
Radoslaw Jachimowicz <radjach@gmail.com>
2021-03-29 09:39:24 +0200

    sw fix ?

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,8 @@ var APP_PREFIX = 'eParking'     // Identifier for this app (this needs to be con
 var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [                            // Add URL you want to cache in this list.
-  'https://github.com/NeozMatrixa/IoT/',                     // If you have separate JS/CSS files,
-  'https://github.com/NeozMatrixa/IoT/index.html'            // add path to those files here
+  'https://neozmatrixa.github.io/IoT/',                     // If you have separate JS/CSS files,
+  'https://neozmatrixa.github.io/IoT/index.html'            // add path to those files here
 ]
 
 // Respond with cached resources
