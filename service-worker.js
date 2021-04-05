7128878c41f37f7591185c7c41de0067e23b8f70
Radoslaw Jachimowicz <radjach@gmail.com>
2021-04-05 18:56:14 +0200

    Round3

--- service-worker.js
+++ service-worker.js
@@ -1,58 +1,27 @@
-var APP_PREFIX = 'eParking'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
-var CACHE_NAME = APP_PREFIX + VERSION
-var URLS = [                            // Add URL you want to cache in this list.
-  'https://neozmatrixa.github.io/IoT/',                     // If you have separate JS/CSS files,
-  'https://neozmatrixa.github.io/IoT/index.html'            // add path to those files here
-]
-
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
-
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
-    })
-  )
-})
-
-// Cache resources
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(URLS)
-    })
-  )
-})
-
-// Delete outdated caches
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create white list
-      var cacheWhitelist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX)
+const CACHE_NAME = "dowolny-string";
+// List of files which are store in cache.
+let filesToCache = ["/", "styles.css", "images/icon.png","images/icon2.png", "main.js"];
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(filesToCache);
       })
-      // add current cache name to white list
-      cacheWhitelist.push(CACHE_NAME)
-
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheWhitelist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] )
-          return caches.delete(keyList[i])
-        }
-      }))
+      .catch(function (err) {
+        // Snooze errors...
+        // console.error(err);
+      })
+  );
+});
+self.addEventListener("fetch", function (evt) {
+  // Snooze logs...
+  // console.log(event.request.url);
+  evt.respondWith(
+    // Firstly, send request..
+    fetch(evt.request).catch(function () {
+      // When request failed, return file from cache...
+      return caches.match(evt.request);
     })
-  )
-})
+  );
+});
