3c36aaaebbe14b6c9c357398d5311305beffea5e
Radoslaw Jachimowicz <radjach@gmail.com>
2021-04-11 14:05:42 +0200

    try cashe files

--- service-worker.js
+++ service-worker.js
@@ -26,11 +26,34 @@ self.addEventListener("fetch", function (evt) {
   );
 });
 
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open('dowolny-string')
-      .then(function (cache) {
-        return cache.addAll(filesToCache);
+function isSuccessful(response) {
+  return response &&
+    response.status === 200 &&
+    response.type === 'basic';
+}
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response; // Cache hit
+        }
+
+        return fetch(event.request.clone())
+          .then(function (response) {
+            if (!isSuccessful(response)) {
+              return response;
+            }
+
+            caches.open(CACHE_NAME)
+              .then(function (cache) {
+                cache.put(event.request, response.clone());
+              });
+
+            return response;
+          }
+        );
       })
-  );
+    );
 });
