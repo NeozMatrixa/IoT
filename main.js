909dd1892975ccf3ca6bf48487eb526bf80745c2
Radoslaw Jachimowicz <radjach@gmail.com>
2021-04-12 10:06:44 +0200

    Cookie added

--- main.js
+++ main.js
@@ -101,10 +101,11 @@ function logAction(_action, _place) {
   console.log(storageObject.action);
   console.log(storageObject.place);
   console.log(storageObject.date);
-}
 
-cookieStore.get('session_id')
-cookieStore.set({ name: storageObject.action, value: storageObject.place });
+  cookieStore.get('session_id')
+  cookieStore.set({ name: storageObject.action, value: storageObject.place });
+
+}
 
 cookieStore.addEventListener('change', (event) => {
   for (const cookie of event.changed) {
