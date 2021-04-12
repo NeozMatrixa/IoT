a2e03f68a31b6127c310d4bcdfa6cb1b47f44051
Radoslaw Jachimowicz <radjach@gmail.com>
2021-04-12 10:03:59 +0200

    Cookie fix part 8

--- main.js
+++ main.js
@@ -103,8 +103,8 @@ function logAction(_action, _place) {
   console.log(storageObject.date);
 }
 
-await cookieStore.get('session_id')
-await cookieStore.set({ name: storageObject.action, value: storageObject.place });
+cookieStore.get('session_id')
+cookieStore.set({ name: storageObject.action, value: storageObject.place });
 
 cookieStore.addEventListener('change', (event) => {
   for (const cookie of event.changed) {
