b78a31ff601b432378abf230638aabd1793e085b
Radoslaw Jachimowicz <radjach@gmail.com>
2021-04-12 09:29:40 +0200

    Cookie adding

--- service-worker.js
+++ service-worker.js
@@ -57,3 +57,41 @@ self.addEventListener('fetch', function (event) {
       })
     );
 });
+
+
+const SHARED_DATA_ENDPOINT = '/token';
+
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  const {
+    request,
+    request: {
+      url,
+      method,
+    },
+  } = event;
+  if  (url.match(SHARED_DATA_ENDPOINT)) {
+    if (method === 'POST') {
+      request.json().then(body => {
+        caches.open(SHARED_DATA_ENDPOINT).then(function(cache) {
+          cache.put(SHARED_DATA_ENDPOINT, new Response(JSON.stringify(body)));
+        });
+      }); 
+      return new Response('{}');
+    } else {
+      event.respondWith(
+        caches.open(SHARED_DATA_ENDPOINT).then(function(cache) {
+          return cache.match(SHARED_DATA_ENDPOINT).then(function (response) {
+            return response || new Response('{}');;
+          }) || new Response('{}');
+        })
+      );
+    }
+  } else {
+    return event;
+  }
+});
